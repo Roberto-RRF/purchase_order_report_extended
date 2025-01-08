435d5555f8e6fc9cf7395a1804ad415e4eed4d49
Roberto-RRF <69941000+Roberto-RRF@users.noreply.github.com>
2025-01-08 08:07:05 -0600

    upload

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Purchase Order Report Extended',
+    'version': '1.0',
+    'author':'ANFEPI: Roberto Requejo Fernández',
+    'depends': [],
+    'description': """
+    """,
+    'data': [
+        'reports/report_purchase_order_inherit.xml'
+    ],
+    'images': ['static/description/icon.png'],
+    'installable': True,
+    'auto_install': False,
+    "license": "AGPL-3",
+}
+
